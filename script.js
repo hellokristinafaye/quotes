c4221999d52b95b4ce2e0b47364d021e79b55a8d
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-08 15:30:06 -0700

    OMG fighting so hard with this API.

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+
+// Tutorial Code (Zero to Mastery)
 let apiQuotes = [];
 
 // Show New Quote
-function newQuote() {
-    // pick a random quote from apiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-}
+// function newQuote() {
+//     // pick a random quote from apiQuotes array
+//     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+//     console.lot(quote);
+// }
 
 // Get Quotes from API
 async function getQuotes() {
-    const apiURL = 'https://type.fit/api/quotes';
+    const apiURL = 'https://zenquotes.io/api/no-cors/';
     try {
         const response = await fetch(apiURL);
         apiQuotes = await response.json();
-        newQuote();
+        // newQuote();
+        console.log(apiQuotes)
     } catch (error) {
         // Catch Error Here
     }
@@ -21,3 +25,28 @@ async function getQuotes() {
 
 // On load
 getQuotes();
+
+
+
+// Trying Rapid API
+// const url = 'https://quotes-api12.p.rapidapi.com/quotes';
+
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'x-rapidapi-host': 'quotes-api12.p.rapidapi.com'
+//     }
+// };
+
+// async function getQuotes() {
+// try {
+//     const response = await fetch(url, options);
+//     const result = await response.text();
+//     console.log(result);
+//     console.log(options);
+// } catch (error) {
+//     console.error(error);
+//     }
+// };
+
+// getQuotes();
