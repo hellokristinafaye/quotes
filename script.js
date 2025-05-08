8b70fa9eada33cd947afd5cd7e30b7680aa15d89
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-08 14:42:55 -0700

    random quote code added

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let apiQuotes = [];
 
+// Show New Quote
+function newQuote() {
+    // pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+}
+
+// Get Quotes from API
 async function getQuotes() {
     const apiURL = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiURL);
         apiQuotes = await response.json();
-        console.log(apiQuotes);
+        newQuote();
     } catch (error) {
         // Catch Error Here
     }
